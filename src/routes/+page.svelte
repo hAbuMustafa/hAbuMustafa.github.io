28c748d51b583ed111f4db5f19409bbc4545eb09
hAbuMustafa <hosam5553@gmail.com>
2024-01-17 22:07:08 +0200

    added cleanup for the dots interval

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,22 @@
 <script lang="ts">
   function fillDots(node: HTMLSpanElement) {
-    setInterval(() => {
-      node.textContent =
-        node.textContent!.length === 3 ? "" : node.textContent + ".";
+    const dotsInterval = setInterval(() => {
+      node.textContent = node.textContent!.length === 3 ? '' : node.textContent + '.';
     }, 1000);
+
+    return {
+      destroy() {
+        clearInterval(dotsInterval);
+      },
+    };
   }
 </script>
 
 <div class="wrapper">
-
   <h1>Work in progress<span use:fillDots>...</span></h1>
   <p>
-    This website is currently a work on progress. You can suddenly find changes
-    popping everywhere.
+    This website is currently a work on progress. You can suddenly find changes popping
+    everywhere.
   </p>
 </div>
 
@@ -28,6 +32,4 @@
       text-align: center;
     }
   }
-
-
 </style>
