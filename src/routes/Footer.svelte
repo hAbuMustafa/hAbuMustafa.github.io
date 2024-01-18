f4065ce0ce5069905faea5bf28475f3849e20a3f
hosam5553 <hosam5553@gmail.com>
2024-01-18 12:09:02 +0000

    better footer display

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,32 +1,35 @@
 <footer>
-  <p>
-    <b>Looking for inspiration?</b> Check out my
-    <a href="https://github.com/hAbuMustafa">github</a>,
-    <a href="https://www.npmjs.com/~eyecode?activeTab=packages">npm</a>. Or, for
-    live projects, browse my <a href="/bookmarklets">bookmarklets</a> list, or
-    check my
-    <a href="/i18n-finder">internationalization index</a>.
-  </p>
-  <p>
-    Follow me on
-    <a
-      href="https://twitter.com/intent/user?user_id=96835066"
-      target="_blank"
-      title="X or Twitter, whatever you like to call it">twitter</a
-    >,
-    <a href="https://bsky.app/profile/hosam.website" target="_blank">blue sky</a
-    >, or hit me on
-    <a rel="me" href="https://onvo.me/Hos" target="_blank">onvo</a>.
-  </p>
+    <p>
+      <b>Looking for inspiration?</b> Check out my
+      <a href="https://github.com/hAbuMustafa">github</a>,
+      <a href="https://www.npmjs.com/~eyecode?activeTab=packages">npm</a>. Or,
+      for live projects, browse my <a href="/bookmarklets">bookmarklets</a>
+      list, or check my
+      <a href="/i18n-finder">internationalization index</a>.
+    </p>
+    <p>
+      Follow me on
+      <a
+        href="https://twitter.com/intent/user?user_id=96835066"
+        target="_blank"
+        title="X or Twitter, whatever you like to call it">twitter</a
+      >,
+      <a href="https://bsky.app/profile/hosam.website" target="_blank"
+        >blue sky</a
+      >, or hit me on
+      <a rel="me" href="https://onvo.me/Hos" target="_blank">onvo</a>.
+    </p>
 </footer>
 
 <style>
   footer {
-    padding-block-end: 2rem;
+    min-height: 8rem;
     border-block-start: 1px double var(--main-color);
     box-shadow: 0px 0.5rem 20px var(--main-color);
     color: var(--text-color);
     background: var(--main-color);
+    display: grid;
+    place-content: center;
 
     line-height: 1.35rem;
     text-align: center;
@@ -35,5 +38,4 @@
       margin-block-start: 0;
     }
   }
-
 </style>
