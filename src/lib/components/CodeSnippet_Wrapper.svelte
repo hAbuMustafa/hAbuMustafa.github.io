19a05e825a1fb36d73da487b13cbd40a558e83bb
hosam5553 <hosam5553@gmail.com>
2024-01-24 10:09:47 +0000

    created CodeWrapper

---
+++ src/lib/components/CodeSnippet_Wrapper.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    export let open=false;
+</script>
+
+<details {open}>
+  <summary>
+    <slot name="title">View Code</slot>
+  </summary>
+  <slot />
+</details>
+
+<style lang="scss">
+  details {
+    &[open] > summary::marker {
+      content: "📖 ";
+    }
+
+    &:not([open]) > summary::marker {
+      content: "📘 ";
+    }
+
+    summary::before {
+      content: "{ ";
+    }
+
+    summary::after {
+      content: " }";
+    }
+  }
+</style>
