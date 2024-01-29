a78acb33a7bf9167a91bf03fd19d63f961e5e2ff
hosam5553 <hosam5553@gmail.com>
2024-01-29 14:03:35 +0000

    written extend-arabic-query page

--- src/routes/[[lang=lang]]/(portfolio)/extend-arabic-query/+page.svelte
+++ src/routes/[[lang=lang]]/(portfolio)/extend-arabic-query/+page.svelte
@@ -1,1 +1,245 @@
+<script lang="ts">
+  import { Abbr } from "@vividus/svelte";
+  import { extendQuery } from "extend-arabic-query";
+
+  let testText = "عبد الرحمن";
+  let againstText = "عبدالرحمن";
+  let allowedPattern = "[ء-ي]";
+
+  function separateWithHyphens(letters: string) {
+    return letters.split("").join(" - ");
+  }
+</script>
+
 <h1><code>extend-arabic-query</code></h1>
+<small>Check the package on <a href="npm.im/extend-arabic-query">npm</a>.</small
+>
+<p>
+  This npm package provides a function to convert any text written in Arabic
+  letters to a <dfn id="dfn-regex"
+    ><Abbr title="[Reg]ular [Ex]pression" styles="white-space: nowrap;" /></dfn
+  >
+  string that can be used as <code>test</code> or <code>match</code> method paramter
+  against any other string to compare for equality.
+</p>
+<p>
+  Normally, comparing words that contain letters that can have multiple forms
+  like the letter "أ" can give false results. For example, comparing the two
+  forms of writing the name "Ahmad" ("أحمد" and "احمد") against each other,
+  should be <code>true</code>, but it doesn't, even if you tried the strict or
+  abstract equality signs [
+  <code>"أحمد" == "احمد"</code>
+  ], since the two forms of the letter "<small class="pal"> alef</small>" (with
+  and without "<small class="pal"> hamzah</small>") are different characters
+  since they have different unicode codes (namely,
+  <code class="unicode">{"أ".charCodeAt(0).toString(16)}</code>
+  for "أ", and <code class="unicode">{"ا".charCodeAt(0).toString(16)}</code> for
+  "ا").
+</p>
+<p>
+  Also, all the other forms of the letter <small class="pal">alef</small> should
+  map to any of the other forms, since they usually get mistakenly swapped for eachother.
+  Say the same for many other letters.
+</p>
+<p>
+  The current version of the package maps the following letters to their
+  corresponding forms or alternatives:
+</p>
+
+<div class="center">
+  <table>
+    <colgroup>
+      <col class="letter-group" />
+      <col class="group-name" />
+      <col class="substitution" />
+    </colgroup>
+
+    <thead>
+      <tr>
+        <th>Letter Group</th>
+        <th>Group Name</th>
+        <th>Substitution</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr id="haa">
+        <td>{separateWithHyphens("ةه")}</td>
+        <td><small class="pal">haa</small> group</td>
+        <td>same<a href="#footnote-note" id="footnote-mark">*</a></td>
+      </tr>
+      <tr id="alef">
+        <td>{separateWithHyphens("اأإآء")}</td>
+        <td><small class="pal">alef</small> group</td>
+        <td
+          >same, plus, all other forms of <small class="pal">hamza</small>
+          ({separateWithHyphens("ئؤ")})</td
+        >
+      </tr>
+      <tr id="hamza-on-yaa">
+        <td>ئ</td>
+        <td><small class="pal">hamza on yaa</small> group</td>
+        <td
+          >same as for <small class="pal">alef</small>, plus all other form of
+          <small class="pal">yaa</small>
+          ({separateWithHyphens("يى")})</td
+        >
+      </tr>
+      <tr id="hamza-on-waw">
+        <td>ؤ</td>
+        <td><small class="pal">hamza on waw</small> group</td>
+        <td
+          >same as for <small class="pal">alef</small>, plus all other form of
+          <small class="pal">yaa</small>
+          ({separateWithHyphens("يى")})</td
+        >
+      </tr>
+      <tr id="waw">
+        <td>و</td>
+        <td><small class="pal">waw</small> group</td>
+        <td> {separateWithHyphens("وؤ")}</td>
+      </tr>
+      <tr id="yaa">
+        <td>{separateWithHyphens("يى")}</td>
+        <td><small class="pal">yaa</small> group</td>
+        <td>same, plus "ئ"</td>
+      </tr>
+    </tbody>
+    <tfoot>
+      <tr>
+        <td colspan="10" id="footnote-note">
+          <a href="#footnote-mark">*</a> "same" means a
+          <a href="#dfn-regex">RegEx</a>
+          list of all the characters, i.e. if any of either "ه" or "ة" was found
+          in the string, it will be replaced with
+          <code>[ةه]</code> in the <a href="#dfn-regex">RegEx</a> string.
+        </td>
+      </tr>
+    </tfoot>
+  </table>
+</div>
+<p>
+  The library also takes account for possible mis-spellings due to local
+  pronounciation. Currently, the following groups are considered:
+</p>
+
+<div class="center">
+  <table>
+    <colgroup>
+      <col class="letter-group" />
+      <col class="group-name" />
+      <col class="substitution" />
+    </colgroup>
+
+    <thead>
+      <tr>
+        <th>Letter Group</th>
+        <th>Group Name</th>
+        <th>Substitution</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr id="zee">
+        <td>{separateWithHyphens("زذ")}</td>
+        <td><small class="pal">zee</small> group</td>
+        <td>same, since many Arabic dialects use both interchangeably</td>
+      </tr>
+      <tr id="see">
+        <td>{separateWithHyphens("ثس")}</td>
+        <td><small class="pal">see</small> group</td>
+        <td>same, since many Arabic dialects use both interchangeably</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+<p>
+  Also, since many would write the words "أبو" and "عبد" with a trailing space
+  or without it. It's been taken into consideration as well!
+</p>
+<p>
+  Also, not to forget irregularly spelled names, like "داوود" and "يحيى" as some
+  may write them with different amount of vowels. This, too, has been taken into
+  consideration!
+</p>
+
+<h2>Try IT!</h2>
+<p>
+  Write whatever you want in this input, and it will immediately show you the
+  RegEx string output beneath it.
+</p>
+
+<div class="center">
+  <input
+    pattern={allowedPattern}
+    type="text"
+    name="text"
+    id="text"
+    bind:value={testText}
+    title="Only Arabic letters allowed"
+  />
+  <span dir="rtl" style:text-align="center">
+    {#if testText && RegExp(allowedPattern, "g").test(testText)}
+      {extendQuery(testText)}
+    {/if}
+  </span>
+  <input
+    pattern={allowedPattern}
+    type="text"
+    name="against-text"
+    id="against-text"
+    bind:value={againstText}
+    title="Only Arabic letters allowed"
+  />
+</div>
+
+<style lang="scss">
+  .pal {
+    /* short for: Pronounced Arabic Letter*/
+    text-transform: uppercase;
+  }
+
+  .unicode::before {
+    content: "U+0";
+  }
+
+  .center {
+    display: grid;
+    place-content: center;
+  }
+
+  table {
+    border-collapse: collapse;
+    max-width: 60vw;
+
+    .letter-group {
+      width: 4rem;
+    }
+
+    th,
+    td {
+      border: 1px solid var(--main-color);
+      padding: 0.5rem;
+      text-align: center;
+    }
+
+    tfoot td {
+      border: none;
+    }
+
+    a[href*="footnote"],
+    a[href*="dfn"] {
+      text-decoration: none;
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+
+  input:invalid {
+    background-color: salmon;
+    outline: 2px dashed red;
+  }
+</style>
