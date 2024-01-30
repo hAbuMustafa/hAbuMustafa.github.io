a8983996fdb7bab1a4f74eaee19b8cfcf37f04e9
hosam5553 <hosam5553@gmail.com>
2024-01-30 14:00:23 +0000

    created What is this component

---
+++ src/lib/components/WhatIsThis.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let href: string;
+</script>
+
+<sup title="What is this?">
+  <a {href}>
+    <small>?</small>
+  </a>
+</sup>
+
+<style>
+  a {
+    text-decoration: none;
+    padding-inline: 0.125rem;
+  }
+</style>
