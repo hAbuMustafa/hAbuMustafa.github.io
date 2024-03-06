1ec828ade6fdd340088903114c8b5a0ca3295465
hAbuMustafa <hosam5553@gmail.com>
2024-03-06 22:49:07 +0200

    fix filter if no url lang param

--- src/routes/[[lang=lang]]/(blog)/blog/+page.server.ts
+++ src/routes/[[lang=lang]]/(blog)/blog/+page.server.ts
@@ -3,7 +3,11 @@ import { getBlogPostList } from '$lib/helpers/blog-posts-helpers';
 export function load({ params }) {
   const blogPostList = getBlogPostList().filter((post) =>
     // if the user navigated to a specified language url, only show posts in that language. Otherwise, default to english posts.
-    !params.lang ? !post.slug.includes('.') : post.slug.endsWith(`.${params.lang}`)
+    !params.lang
+      ? !post.slug.includes('.')
+      : params.lang === 'en'
+      ? !post.slug.match(/\.[a-z]{2}$/)
+      : post.slug.endsWith(`.${params.lang}`)
   );
 
   return { blogPostList };
