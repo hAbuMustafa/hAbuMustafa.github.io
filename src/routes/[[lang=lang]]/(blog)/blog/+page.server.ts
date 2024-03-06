9a6eef6fb8b9a1e5cb6afb61558d9e540c7a1527
hAbuMustafa <hosam5553@gmail.com>
2024-03-07 01:15:55 +0200

    implemented tag search & template translations

--- src/routes/[[lang=lang]]/(blog)/blog/+page.server.ts
+++ src/routes/[[lang=lang]]/(blog)/blog/+page.server.ts
@@ -1,7 +1,38 @@
 import { getBlogPostList } from '$lib/helpers/blog-posts-helpers';
 
-export function load({ params }) {
-  const blogPostList = getBlogPostList().filter((post) =>
+const translations = {
+  title: {
+    en: 'Blog Posts',
+    ar: 'المقالات',
+  },
+  no_results_found: {
+    en: 'No results found',
+    ar: 'لم يتم العثور على نتائج',
+  },
+  try: {
+    en: 'Try',
+    ar: 'حاول أن تقوم بـ',
+  },
+  clearing: {
+    en: 'clearing',
+    ar: 'مسح',
+  },
+  search_params: {
+    en: 'search params',
+    ar: 'كلمات البحث',
+  },
+  currently: {
+    en: 'currently',
+    ar: 'حاليا',
+  },
+  q: {
+    en: '?',
+    ar: '؟',
+  },
+};
+
+export function load({ params, url }) {
+  const postsForLang = getBlogPostList().filter((post) =>
     // if the user navigated to a specified language url, only show posts in that language. Otherwise, default to english posts.
     !params.lang
       ? !post.slug.includes('.')
@@ -10,5 +41,24 @@ export function load({ params }) {
       : post.slug.endsWith(`.${params.lang}`)
   );
 
-  return { blogPostList };
+  const queryTags = url.searchParams.get('tags');
+  const queryTagsArray = queryTags?.split(',') || [];
+
+  const posts = queryTags
+    ? postsForLang.filter((post) =>
+        post.tags.some((postTag) => queryTagsArray.includes(postTag))
+      )
+    : postsForLang;
+
+  return {
+    posts,
+    // title: translations.title[(params.lang as keyof typeof translations.title) || 'en'],
+    translations: Object.fromEntries(
+      Object.entries(translations).map(([key, obj]) => [
+        key,
+        obj[(params.lang as keyof typeof translations.title) || 'en'],
+      ])
+    ),
+    searchParams: queryTagsArray,
+  };
 }
--- src/routes/[[lang=lang]]/(blog)/blog/+page.svelte
+++ src/routes/[[lang=lang]]/(blog)/blog/+page.svelte
@@ -1,10 +1,35 @@
 <script lang="ts">
+  import { page } from '$app/stores';
   import BlogPostExcerpt from './BlogPostExcerpt.svelte';
 
   export let data;
 </script>
 
-<h1>Blog Posts</h1>
-{#each data.blogPostList as post}
-  <BlogPostExcerpt {post} />
-{/each}
+<h1>{data.translations.title}</h1>
+<main>
+  {#if data.posts.length === 0}
+    <p>
+      {data.translations.no_results_found}.
+      {#if data.searchParams.length > 0}
+        {data.translations.try}
+        <a href="{$page.params.lang ? `/${$page.params.lang}` : ''}/blog"
+          >{data.translations.clearing}</a
+        >
+        {data.translations.search_params}{data.translations.q} ({data.translations
+          .currently}: {data.searchParams.join(', ')})
+      {/if}
+    </p>
+  {:else}
+    {#each data.posts as post}
+      <BlogPostExcerpt {post} />
+    {/each}
+  {/if}
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    gap: 2.5rem;
+  }
+</style>
