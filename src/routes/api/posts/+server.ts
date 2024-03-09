bb43b6ec3cdbed5759fdd61923edec92fd98912c
hAbuMustafa <hosam5553@gmail.com>
2024-03-09 22:46:44 +0200

    api endpoint to fetch posts

---
+++ src/routes/api/posts/+server.ts
@@ -0,0 +1,29 @@
+import type { BlogPost, Frontmatter } from '$lib/types/blog-posts';
+import { json } from '@sveltejs/kit';
+
+async function getPosts() {
+  let posts: BlogPost[] = [];
+
+  const paths = import.meta.glob('/src/lib/blog-articles/*.svx', { eager: true });
+
+  for (const path in paths) {
+    const file = paths[path];
+    const slug = path.split('/').at(-1)?.replace('.svx', '');
+
+    if (file && slug && typeof file === 'object' && 'metadata' in file) {
+      const metadata = file.metadata as Frontmatter;
+      const post = { ...metadata, slug } satisfies BlogPost;
+      post.published && posts.push(post);
+    }
+  }
+
+  posts = posts.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+
+  return posts;
+}
+
+export async function GET() {
+  const posts = await getPosts();
+
+  return json(posts);
+}
