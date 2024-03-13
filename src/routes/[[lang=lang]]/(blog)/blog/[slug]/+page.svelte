2ee1f0e791bc5dc6f106ae2a48474e69b13fce4f
Hosam Sultan <hosam5553@gmail.com>
2024-03-13 12:03:06 +0000

    touches for the article appearance

--- src/routes/[[lang=lang]]/(blog)/blog/[slug]/+page.svelte
+++ src/routes/[[lang=lang]]/(blog)/blog/[slug]/+page.svelte
@@ -14,13 +14,29 @@
 
 <article>
   <hgroup>
-    <h1>
+    <h1 id={data.meta.slug}>
       {data.meta.title}
     </h1>
     <p>
-      Published at {formatDate($page.params.lang, data.meta.date)}
+      {formatDate($page.params.lang, data.meta.date)}
     </p>
   </hgroup>
 
   <svelte:component this={data.content} />
 </article>
+
+<style lang="scss">
+  hgroup {
+    margin-block-start: 3rem;
+
+    & > h1 {
+      margin-block-end: 0;
+    }
+
+    & > p {
+      margin-block-start: 0;
+      color: var(--main-color);
+      font-size: 0.75rem;
+    }
+  }
+</style>
