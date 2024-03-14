a6cc64bd5db31b67c98139dd245d60bb0680cf14
hAbuMustafa <hosam5553@gmail.com>
2024-03-14 21:50:16 +0200

    error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+</script>
+
+<hgroup class="error">
+  <h1>
+    <span>{$page.status}:</span>
+    {$page.error?.message}
+  </h1>
+</hgroup>
+
+<style>
+  .error {
+    display: grid;
+    place-content: center;
+    min-height: max-content;
+    height: 90vh;
+    text-align: center;
+  }
+
+  h1 > span {
+    display: block;
+  }
+</style>
