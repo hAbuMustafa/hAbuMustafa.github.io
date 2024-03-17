99d017a2ba55cc494bcb43912231f175142be6ae
hAbuMustafa <hosam5553@gmail.com>
2024-03-17 23:17:46 +0200

    created new filterPosts func

--- src/lib/helpers/utils.ts
+++ src/lib/helpers/utils.ts
@@ -1,3 +1,5 @@
+import type { BlogPost } from '$lib/types/blog-posts';
+
 export function formatDate(locale: string, date: string) {
   return new Date(date).toLocaleDateString(locale, {
     month: 'short',
@@ -5,4 +7,17 @@ export function formatDate(locale: string, date: string) {
     year: 'numeric',
     timeZoneName: 'short',
   });
+}
+
+export function filterPosts(
+  posts: BlogPost[],
+  property: keyof (typeof posts)[0],
+  criteria: string,
+  method?: 'every' | 'some'
+) {
+  if (method) {
+    return posts.filter((post) => post.tags[method]((tag: string) => tag === criteria));
+  } else {
+    return posts.filter((post) => post[property] === criteria);
+  }
 }
