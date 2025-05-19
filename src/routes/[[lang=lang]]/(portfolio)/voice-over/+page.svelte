e031e1341c55017d3680a519a4d221ab555bbc17
hAbuMustafa <hosam5553@gmail.com>
2025-05-20 01:53:13 +0300

    a page for voice over

---
+++ src/routes/[[lang=lang]]/(portfolio)/voice-over/+page.svelte
@@ -0,0 +1,44 @@
+<h1>Voice-Over Portfolio</h1>
+<p>
+  I started working as a voice-over artist in 2019, when I provided a gig to a friend of
+  mine who ran an advertising agency.
+</p>
+<p>
+  Since then, it had dawned on me that I could make a living out of it. Many gigs came
+  after that, and the whole thing shifted into a real interest and enthusiasm, that I took
+  a course on the art, which led me to being selected for the voice-acting industry, where
+  I've been enjoying it the most.
+</p>
+<h2>Audiohat</h2>
+<p>
+  Audiohat is a voice-over agency that I'm a part of, and I'm very proud of it. They
+  provide radio drama, both original works and from the public domain, as well as
+  voice-over for commercials and other projects.
+</p>
+<p>A few projects that I've worked on with Audiohat are:</p>
+<ul>
+  <li>
+    Husien, the main character in the novel series <a
+      href="https://ws.audiohatdar.com/app-sharing?bookid=65d1b3534d64960af2e8bc"
+      >Arzaq (Blessings)</a
+    > by the famous Dr. Nabeel Farouq (Arabic)
+  </li>
+  <li>
+    Sunray, a recurring main character in the novel series <a
+      href="https://ws.audiohatdar.com/app-sharing?bookid=651b2738079be40013a20048"
+      >Fusion</a
+    > by Hamza Fahad Zayed (Arabic)
+  </li>
+</ul>
+<p>
+  Among many other evil roles in projects like <a
+    href="https://ws.audiohatdar.com/app-sharing?bookid=6581e384d2676a0013f4b972"
+    >Felonies</a
+  >,
+  <a href="https://ws.audiohatdar.com/app-sharing?bookid=6669971d62bb5fcde494802d"
+    >The Murder of Farouq El-Sharkawy</a
+  >,
+  <a href="https://ws.audiohatdar.com/app-sharing?bookid=669d60130154291c98ef922b"
+    >A Crime in a New-Cairo Villa</a
+  >, and many more minor roles.
+</p>
