3b7e92e785f98f17d8bff7cfe10daabe5a7c76dc
hAbuMustafa <hosam5553@gmail.com>
2025-05-20 02:45:31 +0300

    migrate to netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import { mdsvex } from 'mdsvex';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import remarkUnwrapImages from 'remark-unwrap-images';
@@ -37,7 +37,10 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: adapter({
+      edge: false,
+      split: false,
+    }),
   },
 };
 
